452f42c9297b218977ba92c7ac94cdcae28be54a
VISHAL DESWAL <44167202+vishaldeswal@users.noreply.github.com>
2022-03-29 18:29:42 +0530

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
-account_sid='AC002dbe648a2859f40156c33214ae2877'
-auth_token='a092cf2ff178f632e185d10c61f0de69'
+account_sid=''
+auth_token=''
 
 def send_sms(msg_body):
     client=Client(account_sid,auth_token)
-    message=client.messages.create(to="+919990055131",from_="+16362514487",body=msg_body)
+    message=client.messages.create(to="+91",from_="+",body=msg_body)
 
 
